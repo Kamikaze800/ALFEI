4cf83b6f958b4803787e979547069cfd7a84e78a
kukold <vlados.18@bk.ru>
2025-03-31 22:47:28 +0700

    футер прижат и ещё по мелачам

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 let burger = document.querySelector(".burger");
 let nav = document.querySelector(".nav");
 let nav__krest = document.querySelector(".nav__krest");
+let bg__nav = document.querySelector(".bg__nav");
 burger.addEventListener("click", () => {
   burger.classList.toggle("burger-active");
   nav.classList.toggle("nav-active");
   nav__krest.classList.toggle("nav__krest-active");
+  bg__nav.classList.toggle("bg__nav-active");
 });
 nav__krest.addEventListener("click", () => {
   burger.classList.toggle("burger-active");
   nav.classList.toggle("nav-active");
   nav__krest.classList.toggle("nav__krest-active");
+  bg__nav.classList.toggle("bg__nav-active");
 });
